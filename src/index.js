4ffbaf481515e2c011929ff2dc8a6d3225543fa7
Mario Gil <mario.gil@wizeline.com>
2018-12-02 22:58:22 +0700

    Remove favorites logic from app component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 import './index.css';
 import Random from './components/Random';
 import Trending from './components/Trending';
@@ -7,57 +9,24 @@ import Favorites from './components/Favorites';
 import { Router } from '@reach/router';
 import Header from './components/Header';
 
-const Routes = ({ onFavorite, favoriteIds, favoriteObjects }) => (
+const Routes = () => (
   <Router>
-    <Trending path="/" onFavorite={onFavorite} favoriteIds={favoriteIds} />
-    <Random path="/random" onFavorite={onFavorite} favoriteIds={favoriteIds} />
-    <Favorites
-      path="/Favorites"
-      onFavorite={onFavorite}
-      FavoriteIds={favoriteIds}
-      Favorites={favoriteObjects}
-    />
+    <Trending path="/" />
+    <Random path="/random" />
+    <Favorites path="/Favorites" />
   </Router>
 );
 
-export default class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      favoriteObjects: [],
-      favoriteIds: [],
-    };
-  }
+const App = () => (
+  <div>
+    <Header />
+    <Routes />
+  </div>
+);
 
-  onFavorite = gif => {
-    let favoriteIds = this.state.favoriteIds;
-    let favoriteObjects = this.state.favoriteObjects;
-    let index = favoriteIds.indexOf(gif.id);
-    if (index === -1) {
-      favoriteIds.push(gif.id);
-      favoriteObjects.push(gif);
-    } else {
-      favoriteIds.splice(index, 1);
-      favoriteObjects.splice(index, 1);
-    }
-    this.setState({
-      favoriteIds,
-      favoriteObjects,
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <Header />
-        <Routes
-          onFavorite={this.onFavorite}
-          favoriteIds={this.state.favoriteIds}
-          favoriteObjects={this.state.favoriteObjects}
-        />
-      </div>
-    );
-  }
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
