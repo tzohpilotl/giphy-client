0d8bee682a558115ebbbdf8c80c627d9c8352589
Mario Gil <mario.gil@wizeline.com>
2018-12-06 16:09:16 +0700

    Add some comments on the reducer

--- src/store/favorites.reducer.js
+++ src/store/favorites.reducer.js
@@ -1,17 +1,23 @@
 export const initialState = {
   favoritesById: {},
-  ids: new Set(),
+  ids: new Set(), // The Set structure will hold unique elements only
 };
 
+// Action creator
+// Remember, an action is just a plain object with at least a `type` property
 export const toggleFavorite = (id, data) => ({
   type: 'TOGGLE_FAVORITE',
   payload: { id, data },
 });
 
+// Reducer
+// A reducer is just a switch statement to choose what will be done to the state
+// based on the action's type
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'TOGGLE_FAVORITE':
       const isFavorite = state.ids.has(action.payload.id);
+      // Make a copy of the mutable parts of your state
       const newIds = new Set(state.ids);
       const newFavorites = { ...state.favoritesById };
 
